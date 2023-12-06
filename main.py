f34a095d92d3506e36040bde8a62e0b642528bc6
Ambika <rudrambika.angadi@docc.techstarter.de>
2023-12-06 23:25:23 +0100

    first tdd commit1

---
+++ main.py
@@ -0,0 +1,7 @@
+def addition(zahl_1, zahl_2):
+    return zahl_1 + zahl_2
+
+def upper_case(string):
+    return string.upper()
+
+
---
+++ test_main.py
@@ -0,0 +1,12 @@
+import pytest
+
+import main as main
+
+def test_addition():
+    result = main.addition(2, 2)
+    assert result == 4
+
+
+def test_upper_case():
+    result = main.upper_case("hello")
+    assert result == "HELLO"
